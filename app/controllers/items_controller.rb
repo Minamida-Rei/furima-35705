3736b1cd54c0915531fb4e9dec72e43221ea9e08
Minamida-Rei <rxnt1120@gmail.com>
2021-06-13 20:47:00 +0900

    商品情報編集機能の修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :new, :edit]
+  before_action :authenticate_user!, only: [:create, :new, :edit, :update]
+  before_action :product_find, only: [:sohw, :edit, :update]
+  before_action :current_user_check, only: [:edit, :update]
+  
   def index
     @product = Product.includes(:user).order("created_at DESC")
   end
@@ -18,31 +21,33 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
-    if user_signed_in? 
-      unless current_user.id == @product.user.id
-        redirect_to action: :index
-      end
-    end
   end
 
   def update
-    @product = Product.find(params[:id])
-    @product.update(product_params)
-    if @product.save
+    if @product.update(product_params)
       redirect_to item_path
     else
       render :edit
     end
   end
-  
+
   private
 
   def product_params
     params.require(:product).permit(:name, :explain, :category_id, :prefecture_id, :status_id, :date_shipment_id, :delivery_charge_id, :value, :image).merge(user_id: current_user.id)
   end
+
+  def product_find
+    @product = Product.find(params[:id])
+  end
+
+  def current_user_check
+    unless current_user.id == @product.user.id
+      redirect_to action: :index
+    end
+  end
+
 end
